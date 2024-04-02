de603685f1eaeb68778e5026af2f226fb40888f6
zettl019 <122923913+zettl019@users.noreply.github.com>
2024-04-02 11:03:07 -0500

    Issue 1 CReadFile
    Created CReadFile class for stack implementation

--- src/CReadFile.h
+++ src/CReadFile.h
@@ -1,4 +1,25 @@
 #ifndef CREADFILE_H
 #define CREADFILE_H
 
+#include <QObject>
+#include <QString>
+#include <QStack>
+
+// Manages reading and organizing movie titles from a text file
+class CReadFile : public QObject
+{
+    Q_OBJECT
+public:
+    explicit CReadFile(QObject *parent = nullptr);
+    void readFile(); // Reads and sorts titles into odd and even stacks
+
+    QStack<QString> getOddStack() const { return stack_odd; }
+    QStack<QString> getEvenStack() const { return stack_even; }
+
+private:
+    QStack<QString> stack_odd;
+    QStack<QString> stack_even;
+};
+
 #endif // CREADFILE_H
+
