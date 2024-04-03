6ff6ed00cdefc6e435a0056fadfc176439ddb014
emmarhoffmann <erhoffmann128@gmail.com>
2024-04-03 08:55:17 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char *argv[]) {
     engine.rootContext()->setContextProperty("fantasyMovies", &fantasyMovies);
 
     // Load GUI QML file
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    engine.load(QUrl(QStringLiteral("qrc:qt/qml/Main/main.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
 
